10b03f8011bf6b358d2a05cc466788497aaa0fcb
mikeonslow <mikeonslow@gmail.com>
2020-07-28 23:09:41 -0400

    Observable lesson final, hot vs cold observables

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,17 @@
-import * as Rx from "rxjs/Observable";
+import { Observable } from "rxjs/Observable";
+import { fromEvent } from 'rxjs/Observable/fromEvent';
 
-console.log(Rx);
+var observable = fromEvent(document, 'mousemove');
+
+setTimeout(()=>{
+    var subscription = observable.subscribe(
+        (x:any) => addItem(x) 
+    )
+}, 2000);
+
+function addItem(val:any) {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById("output").appendChild(node);
+}
